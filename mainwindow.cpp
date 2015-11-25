a0d7f9ff147f8fd491a7ebe550703ac590dbbfd4
Rafał Cebryk <rafal.cebryk@mobica.com>
2015-11-25 14:28:58 +0100

    Basic signals - slots connection

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->actionOpen,SIGNAL(triggered(bool)),SLOT(openSaveFileDialog()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::openSaveFileDialog()
+{
+    QString received = QFileDialog::getSaveFileName(this,"Save File");
+    qDebug() << "Received: " << received;
+}
+
+void MainWindow::on_actionAndCheckBox_triggered()
+{
+    ui->horizontalSlider->setValue(0);
 }
